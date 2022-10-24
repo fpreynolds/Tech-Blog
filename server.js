4576e0e765335a309d0a30541c86861385b24a43
fpr <frankpiercereynolds95@gmail.com>
2022-10-24 19:10:52 -0400

    updated server.js with code

---
+++ server.js
@@ -0,0 +1,44 @@
+const path = require("path");
+const express = require("express"):
+const session = require("express-session");
+const handlebars = require("express-handlebars");
+const helpers = require("./utils/helpers");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sequelize = require("./config/config");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+const sess = {
+    secret: "super secret",
+    cookie: {
+        maxAge: 999999,
+        httpOnly: true,
+        secure: false,
+        sameSite: "strict",
+    },
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(session(sess));
+
+const handlebarHelper = handlebars.create({ helpers });
+
+app.engine("handlebars", handlebarHelper.engine);
+app.set("view engine", "handlebars");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(require("./controllers/"));
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT},`);
+    sequelize.sync({ force: false });
+});
